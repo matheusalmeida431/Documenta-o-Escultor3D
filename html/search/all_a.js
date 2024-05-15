fceae90f7293b70a4e8c19cb0454779fea0b8efb
matheusalmeida431 <106746359+matheusalmeida431@users.noreply.github.com>
2024-05-15 14:56:29 -0300

    Add files via upload

---
+++ html/search/all_a.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['vox_0',['vox',['../class_sculptor.html#a6c5fda1c81b30be5897715551a63a699',1,'Sculptor']]],
+  ['voxel_1',['Voxel',['../struct_voxel.html',1,'']]]
+];
---
+++ html/search/files_0.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['escultor3d_2ecpp_0',['Escultor3D.cpp',['../_escultor3_d_8cpp.html',1,'']]],
+  ['escultor3d_2ehpp_1',['Escultor3D.hpp',['../_escultor3_d_8hpp.html',1,'']]]
+];
